7604867c1ae6de17ea3708289c601da7a9288db6
Davide Canali <79902786+CanaliDavide@users.noreply.github.com>
2021-07-06 11:59:55 +0200

    Project Upload

---
+++ API_Project/cmake-build-debug/CMakeFiles/API_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "API_Project"
+  "API_Project.pdb"
+  "CMakeFiles/API_Project.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/API_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ API_Project/cmake-build-win_profile/CMakeFiles/API_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "API_Project.exe"
+  "API_Project.exe.manifest"
+  "API_Project.pdb"
+  "CMakeFiles/API_Project.dir/main.c.obj"
+  "libAPI_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/API_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
